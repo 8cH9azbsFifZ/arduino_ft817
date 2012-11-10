feb754016ec0df392c527fcc7dfc2651427d4d72
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2012-11-10 11:54:21 +0100

    wip

--- examples/ft_817_remote/t_channels.h
+++ examples/ft_817_remote/t_channels.h
@@ -26,7 +26,7 @@ typedef struct
   char *name; // channel name
   long freq;  // frequency
   byte mode;  // mode
-  int rpt;   // repeater shift
+  long rpt;   // repeater shift
 } t_channel;
 
 const t_channel channels[] = {
