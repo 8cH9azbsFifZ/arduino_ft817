877daa8f223c7e6f0da3cd1ad7ccc3dbca438393
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2012-11-10 12:07:06 +0100

    wip

--- examples/ft_817_remote/prepare_headers.sh
+++ examples/ft_817_remote/prepare_headers.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 bandplan_path=~/src/amafu/bandplan
 xsltproc $bandplan_path/tools/ft817_bandplan.xsl $bandplan_path/bandplans/bandplan.xml > t_bandplan.h
-xsltproc $bandplan_path/tools/ft817_channels.xsl $bandplan_path/bandplans/bandplan.xml > t_channels.h
+xsltproc $bandplan_path/tools/ft817_channels.xsl $bandplan_path/bandplans/bandplan.xml|grep -v "26"|grep -v WSPR|grep -v TV> t_channels.h
