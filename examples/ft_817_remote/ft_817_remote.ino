cdf85ec0daf7efc5c0d6ea2c57e56d874ba026fa
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2012-11-11 11:30:43 +0100

    wip

--- examples/ft_817_remote/ft_817_remote.ino
+++ examples/ft_817_remote/ft_817_remote.ino
@@ -27,7 +27,7 @@
 
 /*************************************************************************************************/
 #define DEBUG 1
-#define DEBUG0 1
+//#define DEBUG0 1
 #define DEBUG1 1
 void initialize_debug ()
 {
@@ -508,7 +508,7 @@ void setup ()
 
   read_rig();
   cur_ch = find_nearest_channel();
-  display_frequency_mode_smeter ();
+  //display_frequency_mode_smeter ();
 }
 
 
